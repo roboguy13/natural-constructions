0fb0da5f6216adc3ab47cc4d2d7b9368c1514e74
David Young <roboguy06@gmail.com>
2015-09-20 23:48:53 -0500

    Formatting

--- src/Data/Natural.hs
+++ src/Data/Natural.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE RankNTypes                #-}
-{-# LANGUAGE TypeOperators             #-} {-# LANGUAGE LiberalTypeSynonyms       #-}
+{-# LANGUAGE TypeOperators             #-}
+{-# LANGUAGE LiberalTypeSynonyms       #-}
 {-# LANGUAGE PolyKinds                 #-}
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE PatternSynonyms           #-}
